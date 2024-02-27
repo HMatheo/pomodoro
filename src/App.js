da675489ce9d41e0ac8331d2c9978104e66a25d9
Mathéo Hersan <matheohersan@MacBook-Pro-de-Matheo.local>
2024-02-27 11:25:41 +0100

    Added files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-cyan-600 text-4xl">Pomodoro</h1>
+    </div>
+  );
+}
+
+export default App;
